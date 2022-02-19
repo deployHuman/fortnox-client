9c63e8668f1a2f1722e7aa1cb67f8b702c16a94c
Gabriel Trosell <gabriel.trosell@gmail.com>
2022-02-19 17:38:35 +0100

    added invoice route

--- src/Api/Fortnox/Invoices.php
+++ src/Api/Fortnox/Invoices.php
@@ -3,6 +3,7 @@
 namespace DeployHuman\fortnox\Api\Fortnox;
 
 use DeployHuman\fortnox\ApiClient;
+use DeployHuman\fortnox\Dataclass\Invoice;
 use DeployHuman\fortnox\Enum\ApiMethod;
 use DeployHuman\fortnox\QueryBuilder\InvoiceParams;
 use GuzzleHttp\Psr7\Response;
@@ -34,5 +35,20 @@ class Invoices extends ApiClient
     {
         if (isset($params)) $params = $params instanceof InvoiceParams ? $params->toArray() : $params;
         return $this->apiWrapper(ApiMethod::GET, '/3/invoices', [], $params);
+    }
+
+    /**
+     * Create an Invoice.
+     * An endpoint for creating an invoice. 
+     * While it is possible to create an invoice without rows, we encourage you to add them if you can. 
+     * Omitted values in the payload will be supplied by Predefined values which can be edited in the Fortnox account settings. 
+     * Note that Predefined values will always be overwritten by values provided through the API.
+     *
+     * @param Invoice $invoice
+     * @return Response
+     */
+    public function apiCreateInvoice(Invoice $invoice): Response
+    {
+        return $this->apiWrapper(ApiMethod::POST, '/3/invoices', $invoice->toArray());
     }
 }
